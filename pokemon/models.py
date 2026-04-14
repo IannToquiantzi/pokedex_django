4dc078499e6d15ee63e2c20b37e49447a8d8259b
Horacio Iann Toquiantzi <ianntoquiantzi020419@hotmail.com>
2026-04-13 22:31:36 -0600

    first commit

---
+++ pokemon/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    base_experience = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
