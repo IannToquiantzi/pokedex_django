889de53ef86314849c9ecd1530153ccaed58937d
Horacio Iann Toquiantzi <ianntoquiantzi020419@hotmail.com>
2026-04-15 13:11:11 -0600

    Changes in the page with Bootstrap

--- pokemon/utils.py
+++ pokemon/utils.py
@@ -5,11 +5,23 @@ def save_pokemon(name):
     data = get_pokemon(name)
     
     if not data:
+        print("No se encontró el Pokémon")
         return None
-    
+
+    try:
+        tipo = data['types'][0]['type']['name']
+        imagen = data['sprites']['front_default']
+        nombre = data['name']
+    except (KeyError, IndexError):
+        print("Error en estructura de API")
+        return None
+
     pokemon, created = Pokemon.objects.get_or_create(
-        name=data['name'],
-        defaults=data
+        nombre=nombre,
+        defaults={
+            "tipo": tipo,
+            "imagen": imagen
+        }
     )
     
     return pokemon
